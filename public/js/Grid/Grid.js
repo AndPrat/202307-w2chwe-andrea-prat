7d5182b99d5691794fb362e7a9b7975f14a49e5f
Andrea Prat <andr8ide@gmail.com>
2023-07-23 16:13:55 +0200

    Change board.js for Grid.js and refactor class

---
+++ public/js/Grid/Grid.js
@@ -0,0 +1,19 @@
+import Cell from "../cell/cell.js";
+class Grid {
+  createGrid(rows, columns) {
+    const grid = [];
+    for (let row = 0; row < rows; row++) {
+      const newRow = [];
+      for (let column = 0; column < columns; column++) {
+        const cell = new Cell();
+        newRow.push(cell);
+      }
+
+      grid.push(newRow);
+    }
+
+    return grid;
+  }
+}
+
+export default Grid;
